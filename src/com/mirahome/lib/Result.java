dec4e853cf8e9d66b948d4c218b9654e44d6a5d9
zhoubin <kyozhou@sina.com>
2018-07-02 15:16:00 +0800

    1. add Result Class

---
+++ src/com/mirahome/lib/Result.java
@@ -0,0 +1,42 @@
+package com.mirahome.lib;
+
+import com.alibaba.fastjson.annotation.JSONField;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Result {
+
+    @JSONField(ordinal = 0)
+    private String error = null;
+    @JSONField(ordinal = 1)
+    private String errorMessage = null;
+    private Object data = null;
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+}
+
